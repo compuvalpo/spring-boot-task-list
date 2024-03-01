68dab6d879b7011c1bf7472b40f85c92cb835cdb
Victor Retamal <compuvalpo@gmail.com>
2024-03-01 12:10:07 -0300

    Java Spring Boot base
    Base del sistema

---
+++ src/test/java/com/compuvalpo/todo/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compuvalpo.todo.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
