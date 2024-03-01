ba56a5f538d875f05bd06e0a1feafe9ba62c7a2f
Victor Retamal <compuvalpo@gmail.com>
2024-03-01 20:54:19 -0300

    Create TaskRepository.java

---
+++ src/main/java/com/compuvalpo/task/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.compuvalpo.task.repository;
+
+import com.compuvalpo.task.model.TaskModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<TaskModel, Integer> {
+}
