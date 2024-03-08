82d9fbc098f2275e4f5cf6c733e71db84112ef28
Victor Retamal <compuvalpo@gmail.com>
2024-03-08 12:10:34 -0300

    Update IndexController.java

--- src/main/java/com/compuvalpo/task/controller/IndexController.java
+++ src/main/java/com/compuvalpo/task/controller/IndexController.java
@@ -24,7 +24,6 @@ public class IndexController {
     @GetMapping("")
     public String index(Model model){
 
-        model.addAttribute("standardDate", new Date());
         model.addAttribute("taskList", this.taskService.findAll());
 
         return "index";
